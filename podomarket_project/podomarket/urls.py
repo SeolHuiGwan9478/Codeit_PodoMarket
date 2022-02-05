132bb8c0ee090df31f478c49ceac5e97500fe239
SeolHuiGwan9478 <shg5977@naver.com>
2022-02-06 02:34:12 +0900

    add delete,update post func

--- podomarket_project/podomarket/urls.py
+++ podomarket_project/podomarket/urls.py
@@ -4,5 +4,7 @@ from .views import *
 urlpatterns = [
     path("", IndexView.as_view(), name='index'),
     path("posts/<int:post_id>/", PostDetailView.as_view(), name="post-detail"),
-    path("post/new/", PostCreateView.as_view(), name="post-create"),
+    path("posts/new/", PostCreateView.as_view(), name="post-create"),
+    path("posts/<int:post_id>/edit/", PostUpdateView.as_view(), name="post-update"),
+    path("posts/<int:post_id>/delete/", PostDeleteView.as_view(), name="post-delete"),
 ]
--- podomarket_project/podomarket/views.py
+++ podomarket_project/podomarket/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from allauth.account.views import PasswordChangeView
 from .models import Post
 from .forms import PostForm
@@ -27,6 +27,23 @@ class PostCreateView(CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = "podomarket/post_form.html"
+    form_class = PostForm
+    pk_url_kwarg = "post_id"
+
+    def get_success_url(self):
+        return reverse('post-detail', kwargs={"post_id": self.object.id})
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = "podomarket/post_confirm_delete.html"
+    pk_url_kwarg = 'post_id'
+
+    def get_success_url(self):
+        return reverse('index')
 
 class CustomPasswordChangeView(PasswordChangeView):
     def get_success_url(self):
